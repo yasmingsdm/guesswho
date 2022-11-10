22d05208655ff3a66ff305ed0409d8bf933cb35e
yasmingsdm <yasmin.dm@gmail.com>
2022-11-10 13:40:11 +0100

    organize images in a folder

--- script.js
+++ script.js
@@ -27,40 +27,40 @@ document.getElementById('showCard').onclick= () =>{
     yourCard.height= 180;
     switch (myCard){
         case'Anna':
-        yourCard.src='./anna.png';
+        yourCard.src='./images/anna.png';
         break;
         case 'Ariel':
-        yourCard.src='./ariel.png';
+        yourCard.src='./images/ariel.png';
         break;
         case 'Belle':
-        yourCard.src='./belle.png';
+        yourCard.src='./images/belle.png';
         break;
         case 'Boo':
-        yourCard.src='./boo.png';
+        yourCard.src='./images/boo.png';
         break;
         case 'Buzz':
-        yourCard.src='./buzz.png';
+        yourCard.src='./images/buzz.png';
         break;
         case 'Elsa':
-        yourCard.src='./elsa.png';
+        yourCard.src='./images/elsa.png';
         break;
         case 'Gaston':
-        yourCard.src='./gaston.png';
+        yourCard.src='./images/gaston.png';
         break;
         case 'Genie':
-        yourCard.src='./genie.png';
+        yourCard.src='./images/genie.png';
         break;
         case 'Merida':
-        yourCard.src='./merida.png';
+        yourCard.src='./images/merida.png';
         break;
         case 'Peter Pan':
-        yourCard.src='./peterpan.png';
+        yourCard.src='./images/peterpan.png';
         break;
         case 'Pinocchio':
-        yourCard.src='./pinocchio.png';
+        yourCard.src='./images/pinocchio.png';
         break;
         case 'Woody':
-        yourCard.src='./woody.png';
+        yourCard.src='./images/woody.png';
         break;
         default:
         console.log ('Ops, something went wrong');
@@ -71,47 +71,47 @@ document.getElementById('showCard').onclick= () =>{
 //Hide your card:
 
 document.getElementById('hideCard').onclick = ()=>{
-    yourCard.src = "./hide.png"
+    yourCard.src = "./images/hide.png"
 }
 
 // Hide pictures:
 
 const hidePic= pictureName =>{
-    document.getElementById(pictureName).src = './hide.png'
+    document.getElementById(pictureName).src = './images/hide.png'
 }
 
 //Refresh the large board:
 
 document.getElementById('mistake').onclick = ()=>{
-        annaBoard.src='./anna.png';
-        arielBoard.src='./ariel.png';
-        belleBoard.src='./belle.png';
-        booBoard.src='./boo.png';
-        buzzBoard.src='./buzz.png';
-        elsaBoard.src='./elsa.png';
-        gastonBoard.src='./gaston.png';
-        genieBoard.src='./genie.png';
-        meridaBoard.src='./merida.png';
-        peterpanBoard.src='./peterpan.png';
-        pinocchioBoard.src='./pinocchio.png';
-        woodyBoard.src='./woody.png'
+        annaBoard.src='./images/anna.png';
+        arielBoard.src='./images/ariel.png';
+        belleBoard.src='./images/belle.png';
+        booBoard.src='./images/boo.png';
+        buzzBoard.src='./images/buzz.png';
+        elsaBoard.src='./images/elsa.png';
+        gastonBoard.src='./images/gaston.png';
+        genieBoard.src='./images/genie.png';
+        meridaBoard.src='./images/merida.png';
+        peterpanBoard.src='./images/peterpan.png';
+        pinocchioBoard.src='./images/pinocchio.png';
+        woodyBoard.src='./images/woody.png'
 }
 
 //Refresh the small board:
 
 document.getElementById('mistake2').onclick = ()=>{
-    annaBoard2.src='./anna.png'
-    arielBoard2.src='./ariel.png'
-    belleBoard2.src='./belle.png'
-    booBoard2.src='./boo.png'
-    buzzBoard2.src='./buzz.png'
-    elsaBoard2.src='./elsa.png'
-    gastonBoard2.src='./gaston.png'
-    genieBoard2.src='./genie.png'
-    meridaBoard2.src='./merida.png'
-    peterpanBoard2.src='./peterpan.png'
-    pinocchioBoard2.src='./pinocchio.png'
-    woodyBoard2.src='./woody.png'
+    annaBoard2.src='./images/anna.png'
+    arielBoard2.src='./images/ariel.png'
+    belleBoard2.src='./images/belle.png'
+    booBoard2.src='./images/boo.png'
+    buzzBoard2.src='./images/buzz.png'
+    elsaBoard2.src='./images/elsa.png'
+    gastonBoard2.src='./images/gaston.png'
+    genieBoard2.src='./images/genie.png'
+    meridaBoard2.src='./images/merida.png'
+    peterpanBoard2.src='./images/peterpan.png'
+    pinocchioBoard2.src='./images/pinocchio.png'
+    woodyBoard2.src='./images/woody.png'
 }
 
 //Play again:
